61d81226c56990979259f2288f113dd622f1aae5
Niranjan <nbniranjan38@gmail.com>
2016-12-25 09:58:27 -0500

    example commit

---
+++ learnings/Java/mylearnings/src/com/niranjan/example/Example.java
@@ -0,0 +1,10 @@
+package com.niranjan.example;
+
+/**
+ * Created by Niran0303 on 12/25/2016.
+ */
+public class Example {
+    public static void main(String[] args){
+        System.out.println("Hi");
+    }
+}
---
+++ learnings/Java/mylearnings/src/com/niranjan/example2/Example2.java
@@ -0,0 +1,16 @@
+package com.niranjan.example2;
+
+/**
+ * Created by Niran0303 on 12/25/2016.
+ */
+public class Example2 {
+    public static void main(String[] args) {
+        char a = 'B';
+        System.out.println();
+        for (int i=1;i<10;i++){
+            System.out.println(a);
+            a++;
+        }
+    }
+
+}
