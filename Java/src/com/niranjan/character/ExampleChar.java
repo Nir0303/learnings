5b58c262ffb346220baf592c28b9514afc6b8142
Niranjan Addanki <nbniranjan38@gmail.com>
2016-12-24 22:47:34 -0500

    java files

---
+++ Java/src/com/niranjan/character/ExampleChar.java
@@ -0,0 +1,13 @@
+package com.niranjan.character;
+
+/**
+ * Created by Niran0303 on 12/25/2016.
+ */
+public class ExampleChar {
+    public static void main(String[] args){
+        char A='A';
+        System.out.println(A);
+        A++;
+        System.out.println(A);
+    }
+}
